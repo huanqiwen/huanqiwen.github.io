536bec9c60bcfc19e2289dbc9bf9d04604bd5879
Vera-Wen7 <wenhuanqi9@gmail.com>
2024-12-28 11:25:37 +0000

    Deploying to gh-pages from @ Vera-Wen7/Vera-Wen7.github.io@cf2d9226ca9bb55f986b5f6cde1313b6ec399c3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -356,17 +356,6 @@ ninja.data = [{
           window.location.href = "/blog/2015/code/";
         
       },
-    },{id: "post-a-post-with-images",
-      
-        title: "a post with images",
-      
-      description: "this is what included images could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/images/";
-        
-      },
     },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
