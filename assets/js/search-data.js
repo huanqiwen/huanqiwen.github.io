89b019d71bd8edda3c45a537a79256708d1f1d17
Vera-Wen7 <wenhuanqi9@gmail.com>
2024-12-28 04:48:58 +0000

    Deploying to gh-pages from @ Vera-Wen7/Vera-Wen7.github.io@a1490c8892c85ef459941b827db9b0eb27789cea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-huanqi-wen",
-    title: "Huanqi Wen",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
