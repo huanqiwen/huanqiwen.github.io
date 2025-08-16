dab2602767f1a18dbdfbb0cf6e5f2944f7a8e333
huanqiwen <wenhuanqi9@gmail.com>
2025-08-16 09:19:50 +0000

    Deploying to gh-pages from @ huanqiwen/huanqiwen.github.io@43ff8ac10fbbd97c927a0cdc5a7b68a49d5f4e55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "You can also see my [\assets/pdf/Academic_CV_Huanqi_Wen (1).pdf](academic CV) here.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
