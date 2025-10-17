3c9407f8fc3a55efd42993588b75a936429c58c9
huanqiwen <wenhuanqi9@gmail.com>
2025-10-17 13:18:39 +0000

    Deploying to gh-pages from @ huanqiwen/huanqiwen.github.io@f3c1323c08bca6c6b203ddd5000aec324a3f7b8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/documents/";
           },
-        },{id: "notes-toric-geometry-seminar",
+        },{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "notes-toric-geometry-seminar",
           title: 'Toric geometry seminar',
           description: "Graduate-level",
           section: "Notes",handler: () => {
